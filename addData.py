a34876bd1179fd84cc8e05ba28ebf699e680cc65
warloccarlos <warloccarlos@gmail.com>
2023-02-13 17:40:58 +0530

    Minor variable changes

--- addData.py
+++ addData.py
@@ -1,12 +1,12 @@
 import sqlite3
 
-connection = sqlite3.connect('DisposableEmail.db')
+connection = sqlite3.connect('Name of the database.db')
 cursor = connection.cursor()
 
-my_file = open("DisposableEmail.txt", "r")
+my_file = open("Absolute Path of Text file.txt", "r")
 # reading the file
 data = my_file.readlines()
-  
+
 dataList = data
 # printing the data
 #print(dataList[2])
@@ -14,8 +14,8 @@ dataList = data
 for dl in dataList:
     dl = dl.split('\n')
     print(dl[0])        #This is optional for visual confirmation
-    cursor.execute('insert into disposableEmail(emailDomain) values(?)', [(dl[0])])
+    cursor.execute('insert into tablename(emailDomain) values(?)', [(dl[0])])
     connection.commit()
-    
+
 connection.close()
 my_file.close()
